0ed0c1d550344cd9a043cca6ae3201788793f6ee
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2025-05-15 04:05:50 +0900

    flx: Menu 스키마 image 추가

--- models/Menu.js
+++ models/Menu.js
@@ -3,8 +3,13 @@ const Schema = mongoose.Schema;
 
 const menuSchema = Schema({
   name: { type: String, required: true },
+  image: {
+    type: String,
+    default:
+      "https://previews.123rf.com/images/azvector/azvector1803/azvector180300154/97280887-%EC%B7%A8%EC%86%8C-%EA%B8%B0%ED%98%B8-%EC%B9%B4%EB%A9%94%EB%9D%BC-%EC%95%84%EC%9D%B4%EC%BD%98%EC%9E%85%EB%8B%88%EB%8B%A4.jpg",
+  },
   cornerNum: { type: Number, required: true },
-  description: { type: String },
+  description: { type: String, default: "" },
   price: { type: Number, required: true },
 });
 

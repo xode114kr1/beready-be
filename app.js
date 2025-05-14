1117d413feddce2eed08be2fb13f591dbdc985f4
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2025-05-14 17:08:26 +0900

    feat : app.js 구현
    feat : User.js 컬랙션 구현
    
    feat : index.js 최상위 라우터 구현

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const indexRouter = require("./routes/index");
+
+const app = express();
+require("dotenv").config();
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use("/api", indexRouter);
+
+const mongoURL = process.env.LOCAL_DB_ADDRESS;
+mongoose
+  .connect(mongoURL, { userNewUrlParser: true })
+  .then(() => console.log("mongoose connected"))
+  .catch((e) => console.log("DB connection fail", e));
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("server on");
+});
---
+++ models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    level: { type: String, default: "user" },
+  },
+  { timestamps: true }
+);
+
+userSchema.methods.toJSON = function () {
+  const obj = this._doc;
+  delete obj.password;
+  delete obj.__v;
+  delete obj.updatedAt;
+  delete obj.createdAt;
+  return obj;
+};
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
